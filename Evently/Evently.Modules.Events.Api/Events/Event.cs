b5e5ecd4c0bf2d0f31784373c215e15ca863c60b
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-12-05 01:33:27 +0100

    Initial commit

---
+++ Evently/Evently.Modules.Events.Api/Events/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evently.Modules.Events.Api.Events;
+
+public sealed class Event
+{
+    public Guid Id { get; set; }
+    public string Title {  get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
+    public DateTime StartsAtUts { get; set; }
+    public DateTime? EndsAtUts { get; set; }
+    public EventStatus Status { get; set; }
+}
