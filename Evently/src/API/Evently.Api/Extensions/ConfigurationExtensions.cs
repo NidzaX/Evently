d646ae26ab5b0ab66b2194ac689848cc87fd1822
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-12-10 13:20:43 +0100

    Automatically Registering Endpoints

--- Evently/src/API/Evently.Api/Extensions/ConfigurationExtensions.cs
+++ Evently/src/API/Evently.Api/Extensions/ConfigurationExtensions.cs
@@ -4,11 +4,10 @@ internal static class ConfigurationExtensions
 {
     internal static void AddModuleConfiguration(this IConfigurationBuilder configurationBuilder, string[] modules)
     {
-        foreach(string module in modules)
+        foreach (string module in modules)
         {
             configurationBuilder.AddJsonFile($"modules.{module}.json", false, true);
             configurationBuilder.AddJsonFile($"modules.{module}.Development.json", true, true);
-
         }
     }
 }
--- Evently/src/Common/Evently.Common.Application/ApplicationConfiguration.cs
+++ Evently/src/Common/Evently.Common.Application/ApplicationConfiguration.cs
@@ -17,7 +17,7 @@ public static class ApplicationConfiguration
 
             config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
             config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
-            config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));  
+            config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
         });
 
         services.AddValidatorsFromAssemblies(moduleAssemblies, includeInternalTypes: true);
